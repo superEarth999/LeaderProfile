92e989df1de719dd0072d2804e781be175c0a2f0
superEarth999 <earthmernstack@gmail.com>
2024-02-21 09:55:01 +0530

    menu bar fixed

--- script.js
+++ script.js
@@ -16,12 +16,40 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 // menu bar
 
 
+// document.getElementById('menu-toggle').addEventListener('click', function() {
+//     var nav = document.querySelector('nav');
+//     nav.classList.toggle('show');
+//     this.classList.toggle('change');
+
+//     // Prevent scrolling on body when the menu is open
+//     document.body.classList.toggle('no-scroll');
+// });
+
+
 document.getElementById('menu-toggle').addEventListener('click', function() {
     var nav = document.querySelector('nav');
     nav.classList.toggle('show');
     this.classList.toggle('change');
 
-    // Prevent scrolling on body when the menu is open
+    // Toggle body scrolling
     document.body.classList.toggle('no-scroll');
 });
+
+// Close the menu when a menu item is clicked
+document.querySelectorAll('nav ul li a').forEach(function(item) {
+    item.addEventListener('click', function() {
+        var nav = document.querySelector('nav');
+        var menuToggle = document.getElementById('menu-toggle');
+        
+        // Close the menu
+        nav.classList.remove('show');
+        menuToggle.classList.remove('change');
+        
+        // Enable scrolling
+        document.body.classList.remove('no-scroll');
+    });
+});
+
+
+
 
